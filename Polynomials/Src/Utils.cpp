50f55531c0ca9a951cbb56907b92c333ae5fa620
StanciuAlin <stanciualin26@yahoo.com>
2018-12-08 19:34:35 +0200

    Delete useless comment

--- Polynomials/Src/Utils.cpp
+++ Polynomials/Src/Utils.cpp
@@ -5,23 +5,6 @@
 
 bool StartProgram()
 {
-	/*Polynomial P1, P2, P3;
-	Polynomial v1, v2;
-
-	v1 = P1 ^ 2(x)-P2(x) * P3(x);
-	v2 = (P1 ^ 2 - P2 * P3)(x);
-	if (v1 > v2)
-	{
-		std::cout << "Polinomul 1 este mai mare decat polinomul doi\n";
-	}
-	else if (v1 < v2)
-	{
-		std::cout << "Polinumul 1 este mai mic decat polinomul 2\n";
-	}
-	else
-	{
-		std::cout << "Polinomul 1 are toti coeficientii egali cu coeficientii polinomului 2\n";
-	}*/
 	int degree1, degree2;
 	std::cout << "Gradul primului polinom este: ";
 	std::cin >> degree1;
